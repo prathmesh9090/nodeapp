a71b47d7e3d31b1a03a9892abc47f56942ac6107
prathmesh9090 <prathmeshpaware9090@gmail.com>
2022-10-26 18:53:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+
+const port= process.env.PORT || 5000;
+app.use(express.static('public'));
+
+
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.listen(port)
